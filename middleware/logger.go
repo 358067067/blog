7116446d51bc72773914d3152f7692381b271edf
358067067 <xxhundan@qq.com>
2021-01-28 22:24:47 +0800

    blog-log-finish

--- middleware/logger.go
+++ middleware/logger.go
@@ -7,12 +7,47 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	rotatelogs "github.com/lestrrat-go/file-rotatelogs"
+	"github.com/rifflock/lfshook"
 	"github.com/sirupsen/logrus"
 )
 
 //Logger 日志
 func Logger() gin.HandlerFunc {
+	filePath := "log/goblog"
+	//linkName := "latest_log.log"
+	src, err := os.OpenFile(filePath, os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		fmt.Println("err", err)
+	}
+
 	logger := logrus.New()
+	logger.Out = src
+	logger.SetLevel(logrus.DebugLevel)
+
+	logWriter, _ := rotatelogs.New(
+		filePath+"%Y%m%d.log",
+		rotatelogs.WithMaxAge(7*24*time.Hour),
+		rotatelogs.WithRotationTime(24*time.Hour),
+		//软连接，在根目录下生成指向最新日志的日志文件
+		//rotatelogs.WithLinkName(linkName),
+	)
+
+	writeMap := lfshook.WriterMap{
+		logrus.InfoLevel:  logWriter,
+		logrus.FatalLevel: logWriter,
+		logrus.DebugLevel: logWriter,
+		logrus.WarnLevel:  logWriter,
+		logrus.ErrorLevel: logWriter,
+		logrus.PanicLevel: logWriter,
+	}
+
+	hook := lfshook.NewHook(writeMap, &logrus.TextFormatter{
+		TimestampFormat: "2006-01-02 15:04:05", //go语言诞生日期(固定日期格式)
+	})
+
+	logger.AddHook(hook)
+
 	return func(ctx *gin.Context) {
 		startTime := time.Now()
 		//洋葱模式（火炬传递）
